5c29d4fc1b73fee6706939955d915c9e5131612f
roniahamed <mdroniahamed56@gmail.com>
2025-08-12 00:24:42 +0600

    Use Custom User models

--- testapp/models.py
+++ testapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    class UserTypes(models.TextChoices):
+        STUDENT = 'STUDENT', 'Student'
+        TEACHER = 'TEACHER', 'Teacher'
+        PRINCIPAL = 'PRINCIPAL', 'Principal'
+
+    user_type = models.CharField(
+        max_length=30, 
+        choices=UserTypes.choices, default=UserTypes.STUDENT
+    )
