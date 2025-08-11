c4083e42574ca45ff7612e451ca19727d2707b2e
roniahamed <mdroniahamed56@gmail.com>
2025-08-12 00:38:10 +0600

    all deshboard views created

--- testapp/views.py
+++ testapp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def teacher_dashboard(request):
+    return HttpResponse(f"<h1> Welcome to the Teacher's Dashboard, {request.user.first_name} !</h1> ")
+
+def student_dashboard(request):
+    return HttpResponse(f"<h1> Welcome to the Student's Dashboard, {request.user.first_name} !</h1> ")
+
+def principal_dashboard(request):
+    return HttpResponse(f"<h1> Welcome to the Principal's Dashboard, {request.user.first_name} !</h1> ")
